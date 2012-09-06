458a0a4ae5eca855a382401561e1a3b8060a1cdd
Collin Anderson <collin@gmail.com>
2012-09-06 19:39:01 -0400

    better

--- scripts/filternet.py
+++ scripts/filternet.py
@@ -7,11 +7,18 @@ from optparse import OptionParser
 
 '''
         filternet.py
-        Use: filternet.py [hosts file] [outfile file] [trigger]
         -------------------------------------------------
-        Host: a structure for maintaining data structures
-        current:
-        target:
+		Iterates through a list of hosts to attempt to trigger
+		content restrictions or firewalls, then examines:
+			
+			1.) TCP Resets,
+			2.) ICMP Echos,
+			3.) HTTP Responses
+		
+		For TTLs that may give away the position of filtering
+		devices.
+		
+		Acknowlegments:
 '''
 
 '''
@@ -50,45 +57,58 @@ def callback(header, data):
 
         decoder = ImpactDecoder.EthDecoder()
         ethernet_pck = decoder.decode(data)
+
         ip_hdr = ethernet_pck.child()
         tcp_hdr = ip_hdr.child()
 
-        if tcp_hdr.get_RST():
                 source_ip = ip_hdr.get_ip_src()
                 dest_ip = ip_hdr.get_ip_dst()
-                
-                print "TCP Reset Recieved: Around IP:  [ttl: %s]  (Scanning: %s)" % (source_ip, ip_hdr.get_ip_ttl(), current.address)                
-                return
 
-# Open our pcap session
+        if tcp_hdr.get_RST():                
+                print "TCP Reset Received: Around IP:  [ttl: %s]  (Scanning: %s)" % (source_ip, ip_hdr.get_ip_ttl(), current.address)                
 
-sniff = pcapy.open_live(default_device, 1500, 1, 100)
-sniff.setfilter('ip proto \\tcp or \\udp or \\icmp')
+		print tcp_hdr
+        '''
+        if ICMP Permission Denied:                
+                print "ICMP Permission Denied Received: Around IP:  [ttl: %s]  (Scanning: %s)" % (source_ip, ip_hdr.get_ip_ttl(), current.address)                
+        if Blocked Page:                
+                print "HTML Response Received: Around IP:  [ttl: %s]  (Scanning: %s)" % (source_ip, ip_hdr.get_ip_ttl(), current.address)                
+        '''
+		return
 
-thread = watchThread(sniff);
-thread.start()
-
-target = Host()
-target.address  = "www.balatarin.com"
-target.host     = "www.balatarin.com"
-
-current = target
-
-if options.stateless:
-        #packet = IP(dst=target.address)/TCP(dport=80,flags='PA',ack=6785,seq=1)/Raw("GET / HTTP/1.0\nHost: " + target.host + "\r\n\r\n")
-        #sr1(packet, verbose=0)
-        print "stateless"
-else:
-        f = urllib.urlopen('http://' + target.address)
-        print 'Code: ' + str(f.getcode())
-        f.close()
-
-target.traceroute,_ = traceroute(target.address, dport=80, maxttl=default_ttl, verbose=0)
-
-print target.traceroute.get_trace().values()[0]
-print target.traceroute.get_trace().values()[0][1][0]
-
-thread.join()
-
+try:
+	
+	sniff = pcapy.open_live(default_device, 1500, 1, 100)
+	sniff.setfilter('ip proto \\tcp or \\udp or \\icmp')
+	
+	thread = watchThread(sniff);
+	thread.start()
+	
+	target = Host()
+	input_line = "balatarin.com, balatarin.com"
+	(target.address, target.host)  = input_line.split(',')
+	
+	if target.host == '':
+	target.host = target.address
+	
+	current = target
+	
+	if options.stateless:
+		#packet = IP(dst=target.address)/TCP(dport=80,flags='PA',ack=6785,seq=1)/Raw("GET / HTTP/1.0\nHost: " + target.host + "\r\n\r\n")
+		#sr1(packet, verbose=0)
+		print "stateless"
+	else:
+		f = urllib.urlopen('http://' + target.address)
+		print 'HTTP Response Code for %s: %s' + (target.address, str(f.getcode()))
+		f.close()
+	
+	target.traceroute,_ = traceroute(target.address, dport=80, maxttl=default_ttl, verbose=0)
+	
+	print target.traceroute.get_trace().values()[0]
+	print target.traceroute.get_trace().values()[0][1][0]
+	
+	thread.join()
+except KeyboardInterrupt:
+	print 'shutting down'	
 
 
